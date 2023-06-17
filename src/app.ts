028ff177182390af949297a64d9c3c61acbd6516
jeremiah ojo <ojoj665@gmail.com>
2023-06-17 18:06:11 +0100

    cors

--- src/app.ts
+++ src/app.ts
@@ -15,16 +15,16 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(
-//   cors({
-//     origin: ["https://jerryjay.stoplight.io",],
-//     credentials: true,
-//   })
-// );
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  next();
-});
+app.use(
+  cors({
+    origin: ["https://jerryjay.stoplight.io","https://stoplight.io"],
+    credentials: true,
+  })
+);
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   next();
+// });
 
 app.use(limiter)
 app.use('/api/users', userRoute)
