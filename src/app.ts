d2a7d35219fb7d0bae2f84a5cfad10effcb839db
Jeremiah Ojo <ojoj665@gmail.com>
2023-06-21 14:02:58 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 import express from 'express';
 import morgan from 'morgan'
-// import cors from 'cors'
+import cors from 'cors'
 // import * as cors from 
 
 import cookieParser from 'cookie-parser';
@@ -10,14 +10,14 @@ import userRoute from "./routes/userRoute"
 import statesroute from "./routes/datatRoute"
 import { limiter } from './middlewares/limiter';
 
-const cors = require('cors')
-
 
 
 const app = express()
 app.use(morgan('dev'))
 app.use(express.json())
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use((req, res, next) => {
@@ -26,7 +26,7 @@ app.use(cookieParser());
 // });
 
 app.use(limiter)
-app.use('/api/users', userRoute)
-app.use('/api/data', statesroute)
+app.use('/api/users', cors(), userRoute)
+app.use('/api/data', cors(), statesroute)
 app.use(errorHandler);
 export default app
