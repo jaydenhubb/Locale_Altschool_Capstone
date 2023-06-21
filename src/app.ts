3f45ceee443dd4ba8b527aa143d8393a785e4b0b
Jeremiah Ojo <ojoj665@gmail.com>
2023-06-21 18:32:53 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,6 @@ import 'dotenv/config'
 import express from 'express';
 import morgan from 'morgan'
 import cors from 'cors'
-// import * as cors from 
-
 import cookieParser from 'cookie-parser';
 import errorHandler from './middlewares/errorMiddleware';
 import userRoute from "./routes/userRoute"
@@ -16,17 +14,15 @@ const app = express()
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cors({
-  origin: '*'
+  origin: 'https://jerryjay.stoplight.io',
+  credentials: true
 }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   next();
-// });
+
 
 app.use(limiter)
-app.use('/api/users', cors(), userRoute)
-app.use('/api/data', cors(), statesroute)
+app.use('/api/users', userRoute)
+app.use('/api/data', statesroute)
 app.use(errorHandler);
 export default app
